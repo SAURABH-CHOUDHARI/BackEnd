c9d43423939c9ba86a19d6eaf92dfaed22701a5e
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-01-12 19:05:50 +0530

    first commit

---
+++ Htpp Server Creation/script.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const server = http.createServer((req,res) => {
+    console.log('Server running on port 3000');
+    if (req.url === "/") {
+        res.end("home");
+    }
+    else if (req.url === '/about') {
+        res.end("About");
+    }
+    else {
+        res.end('Page Not Found');
+    }
+    
+})
+
+
+
+server.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
